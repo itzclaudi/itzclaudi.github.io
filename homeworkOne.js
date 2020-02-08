6b712e2aa51037437115bef1b067e24b4c83756a
itzclaudi <33404341+itzclaudi@users.noreply.github.com>
2020-02-08 11:58:49 -0800

    Add files via upload

---
+++ homeworkOne.js
@@ -0,0 +1,7 @@
+var numOne, numTwo;
+var numOne = prompt("Enter any whole number");
+var numTwo = prompt("Enter any whole number");
+var numOne = parseInt(numOne);
+var numTwo = parseInt(numTwo);
+var sum = numOne + numTwo;
+alert("You entered " + numOne + " + " + numTwo + " which equals " + sum);
